27c7d56b84e14439f3fabce49c4b4bb85d795be7
TGoncpp <141250817+TGoncpp@users.noreply.github.com>
2024-04-15 11:51:22 +0200

    showing weird function call on glm::perspective

--- Project/vulkanbase/Game.cpp
+++ Project/vulkanbase/Game.cpp
@@ -1201,9 +1201,11 @@ void Game::createDescriptorSetLayout()
 void Game::updateUniformBuffer(uint32_t currentImage)
 {
     UniformBufferObject ubo{};
-    ubo.model = glm::rotate(glm::mat4(1.0f), /*Time::GetElapesedSec() **/ glm::radians(m_RotationSpeed), glm::vec3(0.0f, 0.0f, 1.0f));
-    ubo.view = m_pCamera->CalculateViewMat();
-    ubo.proj  = m_pCamera->CalculateProjMat();
+    //ubo.model = glm::rotate(glm::mat4(1.0f), /*Time::GetElapesedSec() **/ glm::radians(m_RotationSpeed), glm::vec3(0.0f, 0.0f, 1.0f));
+    //ubo.view  = m_pCamera->CalculateViewMat();
+    //ubo.view  = m_pCamera->CalculateViewMat();
+    m_pCamera-> CalculateProjMat();
+    m_pCamera-> CalculateProjMat();
 
     ubo.proj[1][1] *= -1; // flip the y-axis. now it wil be from bottom(0) to top(1)
 
@@ -1663,70 +1665,70 @@ VkSampleCountFlagBits Game::getMaxUsableSampleCount()
 //------------------------------------------------------------
 //Camera
 //-----------------------------------------------------------
-void Camera::keyEvent(int key, int scancode, int action, int mods)
-{
-    const float cameraspeed{ 0.2f };
-    if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS))
-    {
-        m_Position += m_Forward * cameraspeed;
-    }
-    if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS))
-    {
-        m_Position -= m_Forward * cameraspeed;
-    }
-    if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS))
-    {
-        m_Position += m_Right * cameraspeed;
-    }
-    if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS))
-    {
-        m_Position -= m_Right * cameraspeed;
-    }
-
-}
-
-void Camera::mouseMove(GLFWwindow* window, double xpos, double ypos)
-{
-    const float RotateSpeed{ 0.0005f };
-
-    int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
-    if (state == GLFW_PRESS)
-    {
-        float dx = static_cast<float>(xpos) - m_DragStart.x;
-        m_Yaw += dx * RotateSpeed * Time::GetElapesedSec();
-        
-        float dy = static_cast<float>(ypos) - m_DragStart.y;
-        m_Pitch += dy * RotateSpeed * Time::GetElapesedSec();
-
-        m_DragStart = glm::vec2{ xpos, ypos };
-    }
-}
-
-void Camera::mouseEvent(GLFWwindow* window, int button, int action, int mods)
-{
-    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-    {
-        double xpos, ypos;
-        glfwGetCursorPos(window, &xpos, &ypos);
-        m_DragStart.x = static_cast<float>(xpos);
-        m_DragStart.y = static_cast<float>(ypos);
-    }
-}
-
-glm::mat4 Camera::CalculateViewMat()
-{
-    glm::mat4 rotation = glm::rotate(glm::mat4(1.f), m_Yaw, m_Up);
-    rotation = glm::rotate(rotation, m_Pitch, glm::vec3{ 1.f,0.f,0.f });
-
-    m_Forward = glm::normalize(rotation[2]);
-    m_Right = glm::normalize(glm::cross(m_Up, m_Forward));
-
-    return  glm::lookAt(m_Position, m_Position + m_Forward, m_Up);
-}
-
-glm::mat4 Camera::CalculateProjMat()
-{
-    return glm::perspective(m_FieldOfView, m_AspectRatio, m_NearPlane, m_FarPlane);
-
-}
+//void Camera::keyEvent(int key, int scancode, int action, int mods)
+//{
+//    const float cameraspeed{ 0.2f };
+//    if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS))
+//    {
+//        m_Position += m_Forward * cameraspeed;
+//    }
+//    if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS))
+//    {
+//        m_Position -= m_Forward * cameraspeed;
+//    }
+//    if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS))
+//    {
+//        m_Position += m_Right * cameraspeed;
+//    }
+//    if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS))
+//    {
+//        m_Position -= m_Right * cameraspeed;
+//    }
+//
+//}
+//
+//void Camera::mouseMove(GLFWwindow* window, double xpos, double ypos)
+//{
+//    const float RotateSpeed{ 0.0005f };
+//
+//    int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+//    if (state == GLFW_PRESS)
+//    {
+//        float dx = static_cast<float>(xpos) - m_DragStart.x;
+//        m_Yaw += dx * RotateSpeed * Time::GetElapesedSec();
+//        
+//        float dy = static_cast<float>(ypos) - m_DragStart.y;
+//        m_Pitch += dy * RotateSpeed * Time::GetElapesedSec();
+//
+//        m_DragStart = glm::vec2{ xpos, ypos };
+//    }
+//}
+//
+//void Camera::mouseEvent(GLFWwindow* window, int button, int action, int mods)
+//{
+//    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+//    {
+//        double xpos, ypos;
+//        glfwGetCursorPos(window, &xpos, &ypos);
+//        m_DragStart.x = static_cast<float>(xpos);
+//        m_DragStart.y = static_cast<float>(ypos);
+//    }
+//}
+//
+//glm::mat4 Camera::CalculateViewMat()
+//{
+//    glm::mat4 rotation = glm::rotate(glm::mat4(1.f), m_Yaw, m_Up);
+//    rotation = glm::rotate(rotation, m_Pitch, glm::vec3{ 1.f,0.f,0.f });
+//
+//    m_Forward = glm::normalize(rotation[2]);
+//    m_Right = glm::normalize(glm::cross(m_Up, m_Forward));
+//
+//    return  glm::lookAt(m_Position, m_Position + m_Forward, m_Up);
+//}
+//
+//glm::mat4 Camera::CalculateProjMat()
+//{
+//    return glm::perspective(m_FieldOfView, m_AspectRatio, m_NearPlane, m_FarPlane);
+//
+//}
 
--- Project/vulkanbase/newCamera.cpp
+++ Project/vulkanbase/newCamera.cpp
@@ -1,72 +1,75 @@
-//#include "newCamera.h"
-//#include <iostream>
-//#define GLFW_INCLUDE_VULKAN
-////#include <GLFW/glfw3.h>
-//#include <glm-1.0.0/glm/gtc/matrix_transform.hpp>
-//#define GLM_FORCE_RADIANS
-//
-//void Camera::keyEvent(int key, int scancode, int action, int mods)
-//{
-//	if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS))
-//	{
-//		//m_CameraPos.x = std::max(3.0f, m_CameraPos.x - 0.2f);
-//		m_Position.x -= 0.2f;
-//	}
-//	if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS))
-//	{
-//		m_Position.x = std::min(30.0f, m_Position.x + 0.2f);
-//	}
-//	if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS))
-//	{
-//		m_Position.y = std::max(3.0f, m_Position.y - 0.2f);
-//	}
-//	if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS))
-//	{
-//		m_Position.y = std::min(30.0f, m_Position.y + 0.2f);
-//	}
-//
-//}
-////
-////void Camera::mouseMove(GLFWwindow* window, double xpos, double ypos)
-//{
-//	int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
-//	if (state == GLFW_PRESS)
-//	{
-//		float dx = static_cast<float>(xpos) - m_DragStart.x;
-//		if (dx > 0) {
-//			m_Yaw += 0.01f;
-//		}
-//		else {
-//			m_Yaw -= 0.01f;
-//		}
-//	}
-//}
-////
-////void Camera::mouseEvent(GLFWwindow* window, int button, int action, int mods)
-//{
-//	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-//	{
-//		std::cout << "right mouse button pressed\n";
-//		double xpos, ypos;
-//		glfwGetCursorPos(window, &xpos, &ypos);
-//		m_DragStart.x = static_cast<float>(xpos);
-//		m_DragStart.y = static_cast<float>(ypos);
-//	}
-//}
-//
-//void Camera::CalculateViewMat()
-//{
-//	glm::mat4 rotation = glm::rotate(glm::mat4(1.f), m_Yaw, glm::vec3{ 0.f, 0.f, 1.f });
-//	rotation = glm::rotate(rotation, m_Pitch, glm::vec3{ 1.f,0.f,0.f });
-//
-//	m_Forward = glm::normalize(rotation[2]);
-//	m_Right = glm::normalize(glm::cross(glm::vec3{ 0.f, 0.f, 1.f }, m_Forward));
-//
-//	m_ViewMat =  static_cast<glm::mat4>(glm::lookAt(m_Position, m_Position + m_Forward, glm::vec3{ 0.f, 0.f, 1.f }));
-//}
-//
-//void Camera::CalculateProjMat()
-//{
-//	m_ProjMat = glm::perspective(m_FieldOfView, m_AspectRatio, m_NearPlane, m_FarPlane);
-//
-//}
+#include "newCamera.h"
+#include <iostream>
+#include "Time.h"
+#define GLFW_INCLUDE_VULKAN
+//#include <GLFW/glfw3.h>
+#include <glm-1.0.0/glm/gtc/matrix_transform.hpp>
+#define GLM_FORCE_RADIANS
+void Camera::keyEvent(int key, int scancode, int action, int mods)
+{
+    const float cameraspeed{ 0.2f };
+    if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        m_Position += m_Forward * cameraspeed;
+    }
+    if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        m_Position -= m_Forward * cameraspeed;
+    }
+    if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        m_Position += m_Right * cameraspeed;
+    }
+    if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        m_Position -= m_Right * cameraspeed;
+    }
+
+}
+
+void Camera::mouseMove(GLFWwindow* window, double xpos, double ypos)
+{
+    const float RotateSpeed{ 0.0005f };
+
+    int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+    if (state == GLFW_PRESS)
+    {
+        float dx = static_cast<float>(xpos) - m_DragStart.x;
+        m_Yaw += dx * RotateSpeed * Time::GetElapesedSec();
+
+        float dy = static_cast<float>(ypos) - m_DragStart.y;
+        m_Pitch += dy * RotateSpeed * Time::GetElapesedSec();
+
+        m_DragStart = glm::vec2{ xpos, ypos };
+    }
+}
+
+void Camera::mouseEvent(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        m_DragStart.x = static_cast<float>(xpos);
+        m_DragStart.y = static_cast<float>(ypos);
+    }
+}
+
+void Camera::CalculateViewMat()
+{
+    glm::mat4 rotation = glm::rotate(glm::mat4(1.f), m_Yaw, m_Up);
+    rotation = glm::rotate(rotation, m_Pitch, glm::vec3{ 1.f,0.f,0.f });
+
+    m_Forward = glm::normalize(rotation[2]);
+    m_Right = glm::normalize(glm::cross(m_Up, m_Forward));
+
+    //return  glm::lookAt(m_Position, m_Position + m_Forward, m_Up);
+    m_ViewMat =  glm::lookAt(m_Position, m_Position + m_Forward, m_Up);
+}
+
+void Camera::CalculateProjMat()
+{
+    //return glm::perspective(m_FieldOfView, m_AspectRatio, m_NearPlane, m_FarPlane);
+    m_ProjMat = glm::perspective(m_FieldOfView, m_AspectRatio, m_NearPlane, m_FarPlane);
+
+}
